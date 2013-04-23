0c8a2bb50af4fcf047fb9cd9764261e390784441
Bryan Kerrigan <bryankerrigan@gmail.com>
2013-04-23 11:13:28 -0700

    Initial fullstay commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fullstay::Application.config.secret_token = '673a8eaa6f8533855cd77889ab908bc6df0fba493d512798c0da5e2baf9e2734fc30cc21e5f27889849366d49eebc00bfe414f5276e665477593affd2c83bf28'
